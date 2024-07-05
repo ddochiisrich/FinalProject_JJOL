8a3e2abefa64ded264ab798de2ba38d2f3386fd9
dudodou <dukim2000@naver.com>
2024-07-05 14:25:53 +0900

    ㅎㅇㅎㅇ

--- project_jjol/src/main/java/com/example/project_jjol/controller/PaymentController.java
+++ project_jjol/src/main/java/com/example/project_jjol/controller/PaymentController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.project_jjol.service.PaymentService;
 
-// hi
+// hi ㅎㅇㅋㅋ
 
 @RestController
 @RequestMapping("/api/payment")
