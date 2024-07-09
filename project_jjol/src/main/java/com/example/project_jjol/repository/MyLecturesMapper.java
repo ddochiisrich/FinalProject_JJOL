c6b7671c92e760eea2357267bec5cf91cbbd407c
kowon <wong11411@gmail.com>
2024-07-09 13:24:20 +0900

    나의 강의 페이지 crud 완료(염시)

--- project_jjol/src/main/java/com/example/project_jjol/repository/MyLecturesMapper.java
+++ project_jjol/src/main/java/com/example/project_jjol/repository/MyLecturesMapper.java
@@ -2,14 +2,36 @@ package com.example.project_jjol.repository;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
+import com.example.project_jjol.model.Chapter;
 import com.example.project_jjol.model.Lecture;
 
 @Mapper
 public interface MyLecturesMapper {
 	@Select("SELECT * FROM Lecture WHERE instructor_id = #{userId}")
     List<Lecture> findMyLecturesByUserId(@Param("userId") String userId);
+	
+	@Delete("DELETE FROM Lecture WHERE lecture_id = #{lectureId}")
+	void deleteLecture(@Param("lectureId") int lectureId);
+	
+	@Select("SELECT * FROM Lecture WHERE lecture_id = #{lectureId}")
+	Lecture findLectureByLectureId(@Param("lectureId") int lectureId);
+	
+	@Select("SELECT * FROM Chapter WHERE lecture_id = #{lectureId}")
+	List<Chapter> findChaptersByLectureId(@Param("lectureId") int lectureId);
+	
+	@Update("UPDATE lecture SET "
+			+ "lecture_title = #{lectureTitle}, "
+			+ "lecture_short_description = #{lectureShortDescription}, "
+			+ "lecture_long_description = #{lectureLongDescription}, "
+			+ "lecture_level = #{lectureLevel}, "
+			+ "lecture_price = #{lecturePrice}, "
+			+ "lecture_discount = #{lectureDiscount} "
+			+ "WHERE lecture_id = #{lectureId}")
+	void updateLecture(Lecture lecture);
 }
