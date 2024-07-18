e502e62ff226199e09679ba4d129cdb0d0fe1bf2
kowon <wong11411@gmail.com>
2024-07-18 15:23:08 +0900

    paymentList 추가

--- project_jjol/src/main/java/com/example/project_jjol/controller/PaymentController.java
+++ project_jjol/src/main/java/com/example/project_jjol/controller/PaymentController.java
@@ -1,12 +1,15 @@
 package com.example.project_jjol.controller;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +17,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.project_jjol.model.Payment;
 import com.example.project_jjol.model.User;
 import com.example.project_jjol.service.PaymentService;
+
+import jakarta.servlet.http.HttpSession;
 
 @RestController
 public class PaymentController {
